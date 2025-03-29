8ff91858bb64b6bf3b1a24c674958da4abe5faba
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 22:09:40 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int idade;
+
+Console.WriteLine("Insira sua idade:");
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade < 18)
+{
+    Console.WriteLine("Você ainda é muito novo.");
+}
+else if (idade < 67)
+{
+    Console.WriteLine("Você está apto a doar");
+}
+else
+{
+     Console.WriteLine("Você não pode ser doador");
+}
